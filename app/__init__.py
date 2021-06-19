88c9fbffe9a405da801fe0ebbc2334d4fded6ddd
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-19 22:30:43 +0300

    registers blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ from flask_simplemde import SimpleMDE
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
-#instances of flask extensions
+# Instances of flask extensions
+# Instance of LoginManger and using its methods
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -15,15 +16,36 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 simple = SimpleMDE()
 mail = Mail()
-#initialize application
+
+#initializing app
+
 photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app=Flask(__name__)
-
-    simple.init_app(app) 
-
-    # creating app configurations 
-    app.config.from_object(config_options[config_name])
     
+    simple.init_app(app)
 
-from app import views 
+       # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+      
+       # Initialising flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app) 
+    login_manager.init_app(app)
+    mail.init_app(app)
+     # configure UploadSet
+    configure_uploads(app,photos) 
+        #configuration options
+    app.config.from_object(config_options[config_name])
+        # Regestering the main blueprint
+    from .main import main as main_blueprint 
+        #register blueprint
+    app.register_blueprint(main_blueprint) 
+
+        # Registering the auth bluprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    return app    
+
+
