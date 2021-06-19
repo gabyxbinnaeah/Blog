29a8afc72cc2cab5035f5f1615bfc3d7259842dd
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-20 00:10:55 +0300

    creates user model

--- app/models.py
+++ app/models.py
@@ -108,38 +108,38 @@ class Downvotes(db.Model):
 
 
 
-# class User(UserMixin, db.Model):
-#     '''
-#     models that dfeines properties of user class
-#     '''
-#     __tablename__="users"
+class User(UserMixin, db.Model):
+    '''
+    models that dfeines properties of user class
+    '''
+    __tablename__="users"
 
-#     id=db.Column(db.Integer,primary_key=True)
-#     username=db.Column(db.String(255))
-#     email=db.Column(db.String(),unique = True,index = True)
-#     profile_pic_path = db.Column(db.String())
-#     bio = db.Column(db.String(255))
-#     password_hash=db.Column(db.String(255))
-#     blogs=db.relationship('Blog', backref ='pitches',lazy= "dynamic")
-#     pitchcomments=db.relationship('PitchComments',backref ='pitchcomment',lazy= "dynamic")
-#     downvotes=db.relationship('Downvotes',backref='dislikes',lazy= "dynamic")
-#     upvotes=db.relationship('Upvotes',backref='likes',lazy= "dynamic")
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(255))
+    email=db.Column(db.String(),unique = True,index = True)
+    profile_pic_path = db.Column(db.String())
+    bio = db.Column(db.String(255))
+    password_hash=db.Column(db.String(255))
+    blogs=db.relationship('Blog', backref ='blogs',lazy= "dynamic")
+    bolgcomments=db.relationship('BlogComments',backref ='bolgcomments',lazy= "dynamic")
+    downvotes=db.relationship('Downvotes',backref='dislikes',lazy= "dynamic")
+    upvotes=db.relationship('Upvotes',backref='likes',lazy= "dynamic")
 
 
-#     @property
-#     def password(self):
-#         raise AttributeError('You can not acess  the password attribute')
+    @property
+    def password(self):
+        raise AttributeError('You can not access  the password attribute')
 
-#     @password.setter
-#     def  password(self,password):
-#         self.password_hash=generate_password_hash(password)
+    @password.setter
+    def  password(self,password):
+        self.password_hash=generate_password_hash(password)
 
-#     def verify_password(self,password):
-#         return check_password_hash(self.password_hash,password)
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     
-#     def __repr__(self):
-#         return f'User {self.username}'
+    def __repr__(self):
+        return f'User {self.username}'
 
  
 
