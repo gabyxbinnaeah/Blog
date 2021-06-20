3430542332602e87e140d65d11c9961f616d8ffc
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-20 08:16:15 +0300

    creates function that validates user email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -16,4 +16,8 @@ class RegistrationForm(FlaskForm):
     password_confirm= PasswordField('Confirm passwords', validators=[Required()])
     submit= SubmitField('Sign Up')
 
+    def validate_email(self,data_field):
+            if User.query.filter_by(email= data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
     
