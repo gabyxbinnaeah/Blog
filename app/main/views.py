499edd4fcd2c9c3cee9f4b7403358f133d2ab7a3
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-20 13:44:03 +0300

    creates functions that handles writer's profile

--- app/main/views.py
+++ app/main/views.py
@@ -32,7 +32,7 @@ def index():
 
 @main.route('/blogs/new/', methods=['GET','POST']) 
 @login_required
-def new_pitcher():
+def new_blog():
     form= BlogForm() 
     my_upvotes.query.filter_by(blog_id=Blog.id)
     if form.validate_on_submi():
@@ -101,6 +101,46 @@ def downvote(blog_id):
 
     new_downvote = Downvote(blog_id=blog_id, user = current_user)
     new_downvote.save_downvotes()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('main.index')) 
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
 
 
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfileForm()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)  
+
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
