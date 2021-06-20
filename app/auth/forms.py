4349c5483386c0d4060d82e3e1357181434e8572
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-20 08:18:42 +0300

    creates function that validates username

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -20,4 +20,8 @@ class RegistrationForm(FlaskForm):
             if User.query.filter_by(email= data_field.data).first():
                 raise ValidationError('There is an account with that email')
 
-    
+    def validate_username(self,data_field):
+        if User.query.filter_by(username= data_field.data).first():
+            raise ValidationError('There is an account with that username')
+
+        
